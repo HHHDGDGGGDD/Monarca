be0bd56c15b3ea5ed2ea8f9d7773945b92916bb5
HHHDGDGGGDD <107428772+HHHDGDGGGDD@users.noreply.github.com>
2022-06-13 13:01:37 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// Breathe...
+// Here in darkness,
+// everything is ok
+// Listen to the waves,
+// and let if fade away...
+//
+// Here comes a thought...
+
+function video(){
+  var div = document.getElementById("youtube");
+  div.classList.remove("oculto")
+}
+
+function cerrar(){   
+  var div = document.getElementById("youtube"); div.classList.add("oculto")
+}
